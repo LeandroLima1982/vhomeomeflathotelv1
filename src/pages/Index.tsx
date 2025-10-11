c22c1bc9b2f125fe5203349f0ce7172c2129bb2c
[dyad] <leandrolima77@gmail.com>
2025-10-10 21:36:13 -0300

    [dyad] Reordenar seções da página inicial - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,23 +1,24 @@
-import About from "@/components/hotel/About";
-import Amenities from "@/components/hotel/Amenities";
-import Contact from "@/components/hotel/Contact";
-import Footer from "@/components/hotel/Footer";
-import Gallery from "@/components/hotel/Gallery";
-import Header from "@/components/hotel/Header";
-import Hero from "@/components/hotel/Hero";
+import React from 'react';
+import Header from '../components/common/Header';
+import Hero from '../components/hotel/Hero';
+import About from '../components/hotel/About';
+import Rooms from '../components/hotel/Rooms';
+import Gallery from '../components/hotel/Gallery';
+import Contact from '../components/hotel/Contact';
+import Footer from '../components/common/Footer';
 
-export default function Index() {
+const Index: React.FC = () => {
   return (
-    <div className="bg-gray-50 text-gray-800">
+    <div>
       <Header />
-      <main>
-        <Hero />
-        <About />
-        <Gallery />
-        <Amenities />
-        <Contact />
-      </main>
+      <Hero />
+      <About />
+      <Gallery />
+      <Rooms />
+      <Contact />
       <Footer />
     </div>
   );
-}
+};
+
+export default Index;
