01d370acc11a2c59240e4df50a348fc9ba0644cd
[dyad] <leandrolima77@gmail.com>
2025-10-10 22:26:37 -0300

    [dyad] Fixing image paths with ?url - wrote 3 file(s)

--- src/components/hotel/About.tsx
+++ src/components/hotel/About.tsx
@@ -5,9 +5,9 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 import Autoplay from "embla-carousel-autoplay";
 import { Star, MapPin, Waves, Wifi, Car, Coffee } from "lucide-react";
 import React from "react";
-import about1 from "@/assets/images/about-1.jpg";
-import about2 from "@/assets/images/about-2.jpg";
-import about3 from "@/assets/images/about-3.jpg";
+import about1 from "@/assets/images/about-1.jpg?url";
+import about2 from "@/assets/images/about-2.jpg?url";
+import about3 from "@/assets/images/about-3.jpg?url";
 
 const images = [about1, about2, about3];
 
