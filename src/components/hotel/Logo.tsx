537838a45ee4845f0fd3757d7e5329ec56c6db81
[dyad] <leandrolima77@gmail.com>
2025-10-12 15:35:10 -0300

    [dyad] Changing logo text color to light gray when header is transparent - wrote 1 file(s)

--- src/components/hotel/Logo.tsx
+++ src/components/hotel/Logo.tsx
@@ -38,7 +38,7 @@ export const Logo: React.FC<LogoProps> = ({ isScrolled = false, isFooter = false
         className={logoClasses}
       />
       <div className="flex flex-col items-center">
-        <h1 className={`font-bold ${isFooter ? 'text-lg' : isScrolled ? 'text-xl' : 'text-2xl'} text-gray-800 dark:text-white`}>
+        <h1 className={`font-bold ${isFooter ? 'text-lg' : isScrolled ? 'text-xl' : 'text-2xl'} ${isScrolled ? 'text-gray-800 dark:text-white' : 'text-gray-300'}`}>
           Flat Hotel
         </h1>
         <div className="flex items-center space-x-1">
