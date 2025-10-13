afac4fc3f38317b793efddc68925f00a21b53aeb
[dyad] <leandrolima77@gmail.com>
2025-10-13 13:08:14 -0300

    [dyad] Corrigir erro de importação do componente - wrote 1 file(s)

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/hotel/Header";
 import { Hero } from "@/components/hotel/Hero";
 import { BookingForm } from "@/components/hotel/BookingForm";
 import { About } from "@/components/hotel/About";
-import { Amenities } from "@/components/hotel/Amenities";
+import Amenities from "@/components/hotel/Amenities";
 import { Nearby } from "@/components/hotel/Nearby";
 import { Gallery } from "@/components/hotel/Gallery";
 import { Footer } from "@/components/hotel/Footer";
