92a3f3b36db702f88da1dd20cbb51cef1ae17d68
[dyad] <leandrolima77@gmail.com>
2025-10-13 01:15:51 -0300

    [dyad] Corrigindo exportação do componente - wrote 1 file(s)

--- src/components/hotel/Rooms.tsx
+++ src/components/hotel/Rooms.tsx
@@ -18,7 +18,7 @@ interface RoomsProps {
   rooms: Room[];
 }
 
-export function Rooms({ rooms }: RoomsProps) {
+export default function Rooms({ rooms }: RoomsProps) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {rooms.map((room) => {
