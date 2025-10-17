69c8e3c75eebb973b87bff37c6b096fecf9223a1
[dyad] <leandrolima77@gmail.com>
2025-10-17 17:56:08 -0300

    [dyad] Ajustando a altura do componente Hero - wrote 1 file(s)

--- src/components/hotel/Hero.tsx
+++ src/components/hotel/Hero.tsx
@@ -92,7 +92,7 @@ export const Hero = () => {
     );
 
   return (
-    <div className="relative h-[75vh] md:h-[60vh] sm:h-[50vh] w-full overflow-hidden">
+    <div className="relative h-[80vh] md:h-[65vh] sm:h-[55vh] w-full overflow-hidden">
       {/* Background Images */}
       {loading ? (
         <div className="absolute inset-0 bg-gray-100" />
