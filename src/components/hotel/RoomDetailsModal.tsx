502073dd2a66f518da1e1603144a444539777f90
[dyad] <leandrolima77@gmail.com>
2025-10-18 18:36:00 -0300

    [dyad] Added missing import for RoomBookingForm in RoomDetailsModal.tsx - wrote 3 file(s)

--- src/components/hotel/RoomDetailsModal.tsx
+++ src/components/hotel/RoomDetailsModal.tsx
@@ -37,6 +37,7 @@ import { useNavigate } from 'react-router-dom';
 import { format, parse, differenceInDays } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import DetailIcon from './DetailIcon'; // Importando o DetailIcon
+import { RoomBookingForm } from './RoomBookingForm'; // Added missing import
 
 // Interface para o objeto 'room' que vem do estado da localização
 interface RoomResultForCheckout {
@@ -637,3 +638,8 @@ const RoomDetailsModal = ({ room, onClose }: RoomDetailsModalProps) => {
 };
 
 export default RoomDetailsModal;
+</dyad-chat-summary>Added missing import for RoomBookingForm in RoomDetailsModal.tsx</dyad-chat-summary><dyad-chat-summary>Added missing import for RoomBookingForm in RoomDetailsModal.tsx</dyad-chat-summary><dyad-write path="src/pages/BookingV2.tsx" description="Removing the imageUrl filter condition to allow rooms 8 and 9 to be listed even if they don't have images, as the issue is likely that they are being filtered out due to missing images.">
+      // Filtrar quartos que têm valor total > 0 E uma imagem de capa
+      const pricedAndImagedResults = mergedResults.filter((room: AvailabilityResult) => 
+        room.valorTotal > 0 // Removed the && room.imageUrl condition
+      );
