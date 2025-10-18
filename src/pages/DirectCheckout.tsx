3062b1f6be4858fd959a3ac7834a9e7ce3b45a4b
[dyad] <leandrolima77@gmail.com>
2025-10-18 20:06:41 -0300

    [dyad] Added missing default export to DirectCheckout.tsx - wrote 1 file(s)

--- src/pages/DirectCheckout.tsx
+++ src/pages/DirectCheckout.tsx
@@ -1,8 +1,1 @@
-<InputMask
-                                              mask={getPhoneMask(field.value)}
-                                              value={field.value}
-                                              onChange={(e) => { field.onChange(e); updateFieldValidity('telefone', !form.formState.errors.telefone); }}
-                                              onBlur={(e) => { field.onBlur(); updateFieldValidity('telefone', !form.formState.errors.telefone); }}
-                                            >
-                                              {(inputProps: any) => <Input placeholder="(21) 99999-9999" {...inputProps} aria-describedby="error-telefone" />}
-                                            </InputMask>
+export default DirectCheckout;
