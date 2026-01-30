db2974799d40c9f82d9344ae629d77bcca2b65a6
[dyad] <leandrolima77@gmail.com>
2026-01-30 20:37:54 -0300

    [dyad] Fixing syntax error in BookingV2 - wrote 1 file(s)

--- src/pages/BookingV2.tsx
+++ src/pages/BookingV2.tsx
@@ -257,7 +257,7 @@ const BookingV2 = () => {
 };
 
 export default BookingV2;
-</dyad-chat-summary>Fixing missing default export in BookingV2</dyad-chat-summary><dyad-write path="src/pages/BookingV2.tsx" description="Adding the missing default export to fix the module error.">
+</dyad-chat-summary>Fixing syntax error in BookingV2 component</dyad-chat-summary>The error indicates there's a syntax issue in the BookingV2.tsx file. Let me fix this by properly completing the component and adding the default export.<dyad-write path="src/pages/BookingV2.tsx" description="Fixing the syntax error by properly closing the component and adding the default export.">
   );
 };
 
